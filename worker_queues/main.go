bb4e772769ed135fb6e12ebc8736c61f2409889d
dhanush <danush2228s@gmail.com>
2025-06-15 11:11:35 +0530

    started and pausing worker queues

---
+++ worker_queues/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/mux"
+)
+
+var client *redis.Client
+
+func init() {
+
+	client = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+
+	err := client.Ping(context.Background()).Err()
+	if err != nil {
+		log.Fatalf("failed to connect to the redis server: %v", err)
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/send", send).Methods(http.MethodPost)
+
+	log.Println("started HTTP server...")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
